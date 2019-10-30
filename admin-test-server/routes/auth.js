db23fc4e22081bff71a6ca170102cfb8d09cc047
phantom05 <monster2jy@gmail.com>
2019-10-31 07:26:21 +0900

    react register query 작성

--- admin-test-server/routes/auth.js
+++ admin-test-server/routes/auth.js
@@ -33,7 +33,7 @@ router.route('/login')
 router.route('/logout')
   .get(async (req, res, next) => {
     console.log('logout');
-    console.log('세션 파괴');
+    console.log('세션 파괴 ');
     res.json({result:1})
   });
 
@@ -73,24 +73,66 @@ router.route('/delete')
   });
 
 
+// router.route('/signup')
+//   .post(async (req, res, next) => {
+//     console.log('signup');
+//     const { email, password } = req.body;
+//     const body = {}
+//     let token = await uuidv4();
+//     token = token.split('-').join('');
+//     console.log('\n');
+//     console.log(token, '****');
+//     console.log(email, password);
+
+//     await query(`INSERT INTO users (email,password,token) VALUES ('${email}', '${password}','${token}')`, (result) => {
+//       if (result.length === 0) {
+//         res.json({ result: 2 })
+//       } else {
+//         res.json({ result: 1 })
+//       }
+//     })
+//   });
+
+
+/**
+ * ROUTER: 백엔드 회원가입 
+ *  // 1번은 회원 가입 완료
+    // 2번은 뭐인가 떄문에 에러.
+    // 4번은 중복체크 
+ */
 router.route('/signup')
   .post(async (req, res, next) => {
-    console.log('signup');
-    const { email, password } = req.body;
-    const body = {}
-    let token = await uuidv4();
-    token = token.split('-').join('');
-    console.log('\n');
-    console.log(token, '****');
-    console.log(email, password);
-
-    await query(`INSERT INTO users (email,password,token) VALUES ('${email}', '${password}','${token}')`, (result) => {
-      if (result.length === 0) {
-        res.json({ result: 2 })
+    const {email,password,username} = req.body;
+    console.log(req.body);
+    let body = {}
+    query(`select * from users where email="${email}"`, (results) => {
+      console.log(results);
+      if (results.length > 0) {
+        res.json({ result: 4 })
       } else {
-        res.json({ result: 1 })
+        console.log('ionin');
+        let token = uuidv4();
+        token = token.split('-').join('');
+        //회원가입 데이터베이스
+        query(`insert into users (email, password, username, token)  
+          values ( "${email}", "${password}", "${username}" ,"${token}")`,
+          function (results) {
+
+            if(results.affectedRows === 1){
+              body.result = 1;
+              body.profile = {}
+              body.profile.email = email;
+              body.profile.token = token;
+              body.profile.username = username;
+            }else{
+              body.result = 2;
+            }
+            res.json(body)
+          })
+
       }
-    })
+    });
   });
+
 
 module.exports = router;
